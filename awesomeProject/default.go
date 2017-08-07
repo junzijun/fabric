79b49d02c1b8f65bd812bad0a491bafd00acabee
jiabo*************************************oo3 <a15592826788@126.com>
2017-08-07 11:54:21 +0800

    2017.8.7

---
+++ awesomeProject/default.go
@@ -0,0 +1,19 @@
+package main
+import(
+	"fmt"
+	"time"
+)
+func main(){
+	tick :=time.Tick(100 *time.Millisecond)
+	boom :=time.After(500 *time.Millisecond)
+	for{
+		select {
+		case<-tick:
+			fmt.Println("BOOM")
+			return
+		default:
+			fmt.Println(".")
+			time.Sleep(50*time.Millisecond)
+		}
+	}
+}
