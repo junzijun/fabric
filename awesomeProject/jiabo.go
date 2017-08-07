90a062559cdb9215d02037cdef0159d64caac3fe
jiabo*************************************oo3 <a15592826788@126.com>
2017-08-07 11:21:08 +0800

    更新

--- awesomeProject/jiabo.go
+++ awesomeProject/jiabo.go
@@ -1,16 +1,6 @@
 package main
-import "fmt"
-func fibonacci(c,quit chan int){
-	x,y:=0,1
-	for{
-		select{
-		case c<-x:
-			x,y =y,x+y
-			case<-quit:
-			fmt.Println("quit")
-			return
-		}
-	}
-}
-func
 
+import "fmt"
+func main(){
+	fmt.Print("aaa")
+}
